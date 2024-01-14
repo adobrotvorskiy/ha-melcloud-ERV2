8b238501edb48e30f13aec0c6e9dab18c6f289f1
adobrotvorskiy <143310339+adobrotvorskiy@users.noreply.github.com>
2024-01-14 19:45:52 +0300

    Update sensor.py

--- custom_components/melcloud_custom/sensor.py
+++ custom_components/melcloud_custom/sensor.py
@@ -5,8 +5,8 @@ from dataclasses import dataclass
 import logging
 from typing import Any, Callable
 
-from pymelcloud import DEVICE_TYPE_ATA, DEVICE_TYPE_ATW, DEVICE_TYPE_ERV
-from pymelcloud.atw_device import Zone
+from pymelcloud_erv import DEVICE_TYPE_ATA, DEVICE_TYPE_ATW, DEVICE_TYPE_ERV
+from pymelcloud_erv.atw_device import Zone
 
 from homeassistant.components.sensor import (
     SensorDeviceClass,

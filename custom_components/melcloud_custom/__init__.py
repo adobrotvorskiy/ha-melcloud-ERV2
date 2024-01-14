9fc83330b26e96b32d6809540e3e95c04a8872c9
adobrotvorskiy <143310339+adobrotvorskiy@users.noreply.github.com>
2024-01-14 22:13:36 +0300

    Update __init__.py

--- custom_components/melcloud_custom/__init__.py
+++ custom_components/melcloud_custom/__init__.py
@@ -8,9 +8,9 @@ from typing import Any, Dict, Optional
 
 from aiohttp import ClientConnectionError, ClientResponseError
 from async_timeout import timeout
-from pymelcloud_erv import Device, get_devices
-from pymelcloud_erv.atw_device import Zone
-from pymelcloud_erv.client import BASE_URL
+from pymelclouderv import Device, get_devices
+from pymelclouderv.atw_device import Zone
+from pymelclouderv.client import BASE_URL
 import voluptuous as vol
 
 from homeassistant.config_entries import SOURCE_IMPORT, ConfigEntry
